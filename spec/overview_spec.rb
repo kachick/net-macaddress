785ed49dc4e05762eeeb3962d5eb4ab62cc8ce52
Kenichi Kamiya <kachick1@gmail.com>
2019-07-09 13:49:20 +0900

    Write simple specs

--- spec/overview_spec.rb
+++ spec/overview_spec.rb
@@ -3,14 +3,42 @@
 require_relative 'spec_helper'
 
 describe Net::MACAddress do
+  describe '.parse' do
+    subject { described_class.parse address }
 
-  before :each do
+    context 'with valid address' do
+      let!(:address) { '04-A3-43-5F-43-23' }
+      
+      it { is_expected.to be_an_instance_of(Net::MACAddress) }
+    end
+
+    context 'with invalid address' do
+      let!(:address) { '04-A3-43-5F-43-23-' }
+      
+      it { expect { subject }.to raise_error(Net::MACAddress::MalformedDataError) }
+    end
   end
 
-  let :let_func do
+  describe '.valid?' do
+    subject { described_class.valid? address }
+
+    context 'with valid address' do
+      let!(:address) { '04-A3-43-5F-43-23' }
+      
+      it { is_expected.to eq(true) }
+    end
+
+    context 'with invalid address' do
+      let!(:address) { '04-A3-43-5F-43-23-' }
+      
+      it { is_expected.to eq(false) }
+    end
   end
 
-  describe "#func" do
+  describe '#==' do
+    it do
+      expect(described_class.parse('04-A3-43-5F-43-23') == described_class.parse('04-A3-43-5F-43-23')).to eq(true)
+      expect(described_class.parse('04-A3-43-5F-43-23') == described_class.parse('04-A3-43-5F-43-24')).to eq(false)
+    end
   end
-
 end
